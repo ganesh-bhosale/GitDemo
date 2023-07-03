44d20d4eab0e01bc8d4b0cf89f47a87fb0251ca1
ganesh-bhosale <ganeshbhosale955@gmail.com>
2023-07-03 19:54:51 +0530

    c.java is added to new cloned repo

---
+++ c.java
@@ -0,0 +1,7 @@
+public class c {
+    public static void main(String [] args){
+        System.out.println("This is c.java file created for Git Demo purpose");
+
+        // Adding some function
+    }
+}
